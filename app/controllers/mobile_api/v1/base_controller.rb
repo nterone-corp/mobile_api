208daa196f1475917f45c4f8e01404b089ef6ea4
Oleg Bavaev <csolg7@gmail.com>
2021-07-01 10:10:42 +1000

    Disable CSRF-Token

--- app/controllers/mobile_api/v1/base_controller.rb
+++ app/controllers/mobile_api/v1/base_controller.rb
@@ -3,4 +3,5 @@ class MobileApi::V1::BaseController < MobileApi::ApplicationController
   before_action :authenticate_user!
   after_action  :verify_authorized
 
+  skip_before_action :verify_authenticity_token
 end
